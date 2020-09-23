67e510586ebbb31967a1e67823bba948355485b4
root5427 <shukrik27@gmail.com>
2020-09-24 02:00:11 +0500

    added: payment status

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,8 +6,19 @@ type Money int64
 // Category represents processed payment category
 type Category string
 
+// Status represents payment status
+type Status string
+
+// Predefined payment statuses
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 type Payment struct {
-	ID int
-	Amount Money
-	Category  Category
+	ID       int
+	Amount   Money
+	Category Category
+	Status   Status
 }
